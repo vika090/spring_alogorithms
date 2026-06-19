2636850b6530c575897f1bf1922bab2f744a6a89
Vika <72411145+vika090@users.noreply.github.com>
2026-06-19 20:09:22 +0000

    итог

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <vector>
+
 #include "task1.hpp"
 
 int main() {
-    int n, Summa;
-    
-    if (!(std::cin >> n >> Summa)) {
-        return 0;
-    }
-    
-    std::vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        std::cin >> arr[i];
-    }
-    
-    std::pair<int, int> result = func(arr, Summa);
+  int S, N;
+  std::cin >> S;
+  std::cin >> N;
+
+  std::vector<int> arr(N);
+  for (int i = 0; i < N; i++) {
+    std::cin >> arr[i];
+  }
+
+  std::pair<int, int> result = func(arr, S);
+
+  if (result.first == -1 && result.second == -1) {
+    std::cout << -1 << std::endl;
+  } else {
     std::cout << result.first << " " << result.second << std::endl;
-    
-    return 0;
+  }
+
+  return 0;
 }
--- task_06/src/main.cpp
+++ task_06/src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
+
 #include "task6.hpp"
 
 int main() {
-    int N, K;
-    std::cin >> N >> K;
-    
-    std::vector<int> arr(N);
-    for (int i = 0; i < N; i++) {
-        std::cin >> arr[i];
-    }
-    
-    std::cout << func(N, K, arr) << std::endl;
-    
-    return 0;
+  int N, K;
+  std::cin >> N >> K;
+
+  std::vector<int> prices(N);
+  for (int i = 0; i < N; i++) {
+    std::cin >> prices[i];
+  }
+
+  std::cout << func(N, K, prices) << std::endl;
+
+  return 0;
 }
